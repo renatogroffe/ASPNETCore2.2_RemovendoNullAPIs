7745ad5f3076ee26831d734c6dd68a0ccfb45f90
Renato Groffe <renato.groffe@yahoo.com.br>
2019-02-07 11:51:23 -0200

    ASP.NET Core 2.2 + Remoção de Nulls em APIs REST
    ASP.NET Core 2.2 + Remoção de Nulls em APIs REST

---
+++ APIProdutos/Controllers/ProdutosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using APIProdutos.Models;
+using APIProdutos.Repository;
+
+namespace APIProdutos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            return ProdutosRepository.Listar();
+        }
+    }
+}
